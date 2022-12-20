740d2057d3d8a84f978715aa4fe9ebfa6b476c9d
Thitichaya Pounglaowech <thitichaya5422@gmail.com>
2022-12-20 17:12:05 +0700

    mkdir

--- get_map.c
+++ get_map.c
@@ -1,26 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   map.h                                              :+:      :+:    :+:   */
+/*   get_map.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tpoungla <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/19 20:40:27 by tpoungla          #+#    #+#             */
-/*   Updated: 2022/12/20 16:44:45 by tpoungla         ###   ########.fr       */
+/*   Created: 2022/12/20 17:10:36 by tpoungla          #+#    #+#             */
+/*   Updated: 2022/12/20 17:10:41 by tpoungla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MAP_H
-# define MAP_H
+#include "map.h"
 
-# include "libft/libft.h"
-# include <stdio.h>
-# include <fcntl.h>
-
-# define TRUE 1
-# define FALSE 0
-
-int	valid_char(char c);
-int	valid_component(char *str);
-
-#endif
